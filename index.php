33cbf9906d727cb793c758355d85a0e578e781ec
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-12-16 23:32:18 +0200

    parse first page auto website

--- index.php
+++ index.php
@@ -2,15 +2,39 @@
 header('Content-type: text/html; charset=utf-8');
 require 'phpQuery.php';
 
-$url = 'https://privatbank.ua/';
+function print_arr($arr)
+{
+  echo '<pre>' . print_r($arr, true) . '</pre>';
+}
+
+$url = 'https://www.kolesa.ru/news';
 $file = file_get_contents($url);
 
-/* $pattern = '#<div class="wr_inner course_type_container.+?</div>#s';
-preg_match($pattern, $file, $matches);
-
-print_r($matches); */
-
 $doc = phpQuery::newDocument($file);
-// $tbl = $doc->find('[data-cource_type="posts_course"] table');
-$tr = $doc->find('[data-cource_type="posts_course"] table tbody tr:eq(2) td:eq(2)')->text();
-echo $tr;
+
+foreach ($doc->find('.row.post-list .post-list-item') as $article) {
+  $article = pq($article);
+
+  // $article->find('.post-category')->remove();
+  // $article->find('.post-category')->prepend('Категория: ');
+  $article->find('.post-category')->wrap('<strong class="category">')->after('Датa: ' . date("Y-m-d H:i:s"));
+
+  $img = $article->find('.post-image');
+
+  foreach ($img as $element) {
+    $element = pq($element);
+    $style = $element->attr('style');
+
+    // Используем регулярное выражение для извлечения URL из строки стиля
+    preg_match('/background-image:\s?url\((.*?)\)/', $style, $matches);
+    if (isset($matches[1])) {
+        $url = $matches[1];
+        // echo $url . '<br/>';
+        echo "<img src={$url}>";
+    }
+  }
+
+  $text = $article->find('.post-content')->html();
+  echo $text;
+  echo '<hr>';
+}
