d10595dd29dc7a80173fe9b80f665dddbf606057
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-12-17 11:46:18 +0200

    parse website use curl from openserver

--- index.php
+++ index.php
@@ -7,9 +7,20 @@ function print_arr($arr)
   echo '<pre>' . print_r($arr, true) . '</pre>';
 }
 
+function get_content($url)
+{
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $res = curl_exec($ch);
+  curl_close($ch);
+  return $res;
+}
+
 function parser($url, $start, $end) {
   if ($start < $end) {
-    $file = file_get_contents($url);
+    // $file = file_get_contents($url);
+    $file = get_content($url);
     $doc = phpQuery::newDocument($file);
 
     foreach ($doc->find('.row.post-list .post-list-item') as $article) {
@@ -43,8 +54,37 @@ function parser($url, $start, $end) {
   }
 }
 
-$url = 'https://www.kolesa.ru/news?page=6751';
+$url = 'https://www.kolesa.ru/news/';
 $start = 0;
 $end = 3;
 
 parser($url, $start, $end);
+
+/* $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_NOBODY, true);
+curl_exec($ch);
+curl_close($ch); */
+
+/* $ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_exec($ch);
+curl_close($ch);  */
+
+/* $fp = fopen("file.txt", "w");
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_FILE, $fp);
+$res = curl_exec($ch);
+curl_close($ch);
+var_dump($res); */
+
+
+/* $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$res = curl_exec($ch);
+curl_close($ch); */
