005990861b41d33ad6a83d7f5312e2311900aa02
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-12-17 10:56:04 +0200

    add step parsing use pagination on website

--- index.php
+++ index.php
@@ -7,34 +7,44 @@ function print_arr($arr)
   echo '<pre>' . print_r($arr, true) . '</pre>';
 }
 
-$url = 'https://www.kolesa.ru/news';
-$file = file_get_contents($url);
+function parser($url, $start, $end) {
+  if ($start < $end) {
+    $file = file_get_contents($url);
+    $doc = phpQuery::newDocument($file);
 
-$doc = phpQuery::newDocument($file);
+    foreach ($doc->find('.row.post-list .post-list-item') as $article) {
+      $article = pq($article);
+    
+      $img = $article->find('.post-image');
+    
+      foreach ($img as $element) {
+        $element = pq($element);
+        $style = $element->attr('style');
+    
+        // Используем регулярное выражение для извлечения URL из строки стиля
+        preg_match('/background-image:\s?url\((.*?)\)/', $style, $matches);
+        if (isset($matches[1])) {
+            $url = $matches[1];
+            echo "<img src={$url}>";
+        }
+      }
 
-foreach ($doc->find('.row.post-list .post-list-item') as $article) {
-  $article = pq($article);
+      $text = $article->find('.post-content')->html();
+      echo $text;
+      echo '<hr>';
+    }
 
-  // $article->find('.post-category')->remove();
-  // $article->find('.post-category')->prepend('Категория: ');
-  $article->find('.post-category')->wrap('<strong class="category">')->after('Датa: ' . date("Y-m-d H:i:s"));
+    $next = $doc->find('.pagination .active')->next()->find('a')->attr('href');
 
-  $img = $article->find('.post-image');
-
-  foreach ($img as $element) {
-    $element = pq($element);
-    $style = $element->attr('style');
-
-    // Используем регулярное выражение для извлечения URL из строки стиля
-    preg_match('/background-image:\s?url\((.*?)\)/', $style, $matches);
-    if (isset($matches[1])) {
-        $url = $matches[1];
-        // echo $url . '<br/>';
-        echo "<img src={$url}>";
+    if (!empty($next)) {
+      $start++;
+      parser($next, $start, $end);
     }
   }
-
-  $text = $article->find('.post-content')->html();
-  echo $text;
-  echo '<hr>';
 }
+
+$url = 'https://www.kolesa.ru/news?page=6751';
+$start = 0;
+$end = 3;
+
+parser($url, $start, $end);
