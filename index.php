9fb32a6f92859b79608ddaa86be27ac1ed56883d
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2023-12-17 12:51:15 +0200

    get content from private page

--- index.php
+++ index.php
@@ -7,84 +7,28 @@ function print_arr($arr)
   echo '<pre>' . print_r($arr, true) . '</pre>';
 }
 
-function get_content($url)
+function get_content($url, $data = [])
 {
   $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+  curl_setopt($ch, CURLOPT_COOKIEJAR, __DIR__ . '/cookie.txt');
+  curl_setopt($ch, CURLOPT_COOKIEFILE, __DIR__ . '/cookie.txt');
   $res = curl_exec($ch);
   curl_close($ch);
   return $res;
 }
 
-function parser($url, $start, $end) {
-  if ($start < $end) {
-    // $file = file_get_contents($url);
-    $file = get_content($url);
-    $doc = phpQuery::newDocument($file);
+$url_auth = 'http://wordpress/wp-login.php';
+$url = 'http://wordpress/zakrytaya-statya/';
+$auth_data = [
+  'log' => 'admin',
+  'pwd' => '123',
+  'rememberme' => 'on',
+];
 
-    foreach ($doc->find('.row.post-list .post-list-item') as $article) {
-      $article = pq($article);
-    
-      $img = $article->find('.post-image');
-    
-      foreach ($img as $element) {
-        $element = pq($element);
-        $style = $element->attr('style');
-    
-        // Используем регулярное выражение для извлечения URL из строки стиля
-        preg_match('/background-image:\s?url\((.*?)\)/', $style, $matches);
-        if (isset($matches[1])) {
-            $url = $matches[1];
-            echo "<img src={$url}>";
-        }
-      }
-
-      $text = $article->find('.post-content')->html();
-      echo $text;
-      echo '<hr>';
-    }
-
-    $next = $doc->find('.pagination .active')->next()->find('a')->attr('href');
-
-    if (!empty($next)) {
-      $start++;
-      parser($next, $start, $end);
-    }
-  }
-}
-
-$url = 'https://www.kolesa.ru/news/';
-$start = 0;
-$end = 3;
-
-parser($url, $start, $end);
-
-/* $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_NOBODY, true);
-curl_exec($ch);
-curl_close($ch); */
-
-/* $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_exec($ch);
-curl_close($ch);  */
-
-/* $fp = fopen("file.txt", "w");
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_FILE, $fp);
-$res = curl_exec($ch);
-curl_close($ch);
-var_dump($res); */
-
-
-/* $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$res = curl_exec($ch);
-curl_close($ch); */
+$data = get_content($url_auth, $auth_data);
+$data = get_content($url);
+var_dump($data);
